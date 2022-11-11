9f1c6c6ee9ecd92746f4dc1a2325668f7d6cf956
MaybeLexter <109309175+MaybeLexter@users.noreply.github.com>
2022-11-11 17:30:10 -0400

    Create estudo.js

---
+++ estudo.js
@@ -0,0 +1,93 @@
+//String = '',"",````.
+//boolean = true false
+//undefined = indefinido
+//null = nulo
+//Object = estrutura (atributos/metodos)
+//array = vetores/lista
+//variaveis = var,let,const
+//var é global/local, funciona fora do scopo
+//scopo {}
+//const e let são locais
+//typeof
+
+/*
+console.log("Hello World${1+1}")
+console.log(12.5)
+console.log({
+    Aluno: 'Leo',
+    Idade: 18
+})
+console.log([
+     'Leo',
+     "Leonardi"
+])
+var aluno = 'Leonardi'
+console.log(aluno)
+let aluno = 'Leonardi'
+console.log(aluno)
+*/
+/*
+const pessoa = {
+      Aluno: 'Leonardi',
+      Idade: 18,
+    
+      Estudando: 'JS'
+}
+console.log(pessoa.Aluno + ' ' + pessoa.Idade)
+console.log(`${pessoa.Aluno} tem ${pessoa.Idade}`)
+*/
+/*
+const Pessoas = [
+    'Amanda',
+    'Natalia',
+    'Natanael'
+]
+console.log(Pessoas[0])
+const Pessoas = [
+    'Amanda',
+    'Natalia',
+    'Natanael',
+    {
+        aluno: 'Leonardi'
+    }
+]
+console.log(Pessoas[3].aluno)
+*/
+/*
+const soma = function(x,y){
+    var r 
+    r = x + y
+    return console.log("O resultado é",r)
+}
+soma(3,5)
+*/
+/*
+soma(3,4)
+function soma(x,y){
+    var r 
+    r = x + y
+    return console.log("O resultado é",r)
+}
+//função elevação
+*/
+/*
+nome = 'Leonardi'
+const Myname = (nome) => {
+    console.log(nome)
+
+
+}
+Myname(nome)
+//Arrow Func
+*/
+/*
+function teste_callback(func) {
+    func()
+}
+teste_callback(
+    () => {
+        console.log('Ola Mundo')
+    }
+)
+//callback func
+*/
